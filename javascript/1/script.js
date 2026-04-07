1287856ca7ca07fe0cf0b15f4710819c5b6f1fae
sderasari <sderasari2@gmail.com>
2026-04-07 14:06:10 +0530

    js

---
+++ javascript/1/script.js
@@ -0,0 +1,20 @@
+console.log(x);
+
+x=90;
+console.log(x);
+
+const pi=3.14;
+console.log(pi);
+
+//pi=2.7 => geneerate error cant reassign value of constatnt
+
+var m=170;
+console.log(m);
+
+var m=20;
+comsole.log(m);
+
+//             let      const      var
+// Redclare    no       no         yes
+// Reassign    yes      no         yes
+
